eab6a02d2834dbab500ca2f2d70cb000bfc7dd73
daniel shlaim <dada373@gmail.com>
2022-06-09 22:52:30 +0300

    Add project files.

---
+++ RouteApi HomeWork/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RouteApi_HomeWork.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route ("{id:int:MinLength(5)}")]
+        public string GetId(int id)
+        {
+            return "My Id Need Minimum 5 Numbers " + id;
+        }
+        
+        [Route("{book:regex(boom|zoom)}")]
+        public string Regex(string book)
+        {
+            return " Regex First Word " + book;
+        }
+    }
+}
